fb418b2a51ad20d5c858e9a7cc27fcc3f14bc470
heya87 <joel.rolli@gmail.com>
2013-12-11 16:38:39 +0100

    added connection between web / java

--- src/java/conv/view/ConvManager.java
+++ src/java/conv/view/ConvManager.java
@@ -9,7 +9,7 @@ import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@Named("acctManager")
+@Named("convManager")
 @ConversationScoped
 public class ConvManager implements Serializable {
 
@@ -40,11 +40,6 @@ public class ConvManager implements Serializable {
         stopConversation();
         e.printStackTrace(System.err);
         transactionFailure = e;
-    }
-
-    private void getOriginValue() {
-        //todo
-        
     }
 
     /**
@@ -84,5 +79,31 @@ public class ConvManager implements Serializable {
         //convFacade.convert(originValue, currency1, currency2);
         System.out.println("value: " + originValue + " curency one: " + currency1 + " currency2: " + currency2);
         return 10;
+    }
+    
+    
+    public void setOriginValue(int originValue){
+        this.originValue = originValue;
+    }
+    
+    public void setCurrency1(String currency1){
+        this.currency1 = currency1;
+    }
+        
+    public void setCurrency2(double originValue){
+        this.currency2 = currency2;
+    }
+    
+    
+    public double getOriginValue(){
+        return originValue;
+    }
+    
+    public String geturrency1(){
+        return currency1;
+    }
+        
+    public String getCurrency2(){
+        return currency2;
     }
 }
