ca3670ca90a3db8f2338c58896e6085e69996c4c
heya87 <joel.rolli@gmail.com>
2014-01-03 13:59:07 +0100

    Login stuff

---
+++ src/java/conv/view/RegisterServlet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package conv.view;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author albert
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    	private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        
+        response.sendRedirect("RegisterSuccess.html");
+}
+    
+}
