61ae7383580930491fba660ca930e6ae06a2016c
albert <albert@localhost>
2014-01-05 22:37:04 +0100

    buy and Cart added

--- src/java/gnomeWebShop/view/UserManager.java
+++ src/java/gnomeWebShop/view/UserManager.java
@@ -8,14 +8,20 @@ package gnomeWebShop.view;
 
 import gnomeWebShop.controller.GWSFacade;
 import gnomeWebShop.model.Gnome;
+import java.awt.event.ActionEvent;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.component.html.HtmlDataTable;
 import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Named;
+import org.omg.CORBA.DomainManager;
 
 /**
  *
@@ -29,7 +35,14 @@ public class UserManager implements Serializable{
     private String password;
    
     private ArrayList<Gnome> gnomesList;
+    
+    private String gnometype;
 
+   
+    private Integer amount = 0;
+    
+    private ArrayList<Gnome> shoppingCart;    
+     
     @EJB
     private GWSFacade gwsFacade;
 
@@ -43,6 +56,7 @@ public class UserManager implements Serializable{
         }
     }
     
+
     public String loginAdmin() {
 
         if(! gwsFacade.login(username, password,true)) {
@@ -75,6 +89,48 @@ public class UserManager implements Serializable{
         gwsFacade.register(username, password, false);
     }
     
+   
+
+        
+   public void setShoppingCart(ArrayList<Gnome> shoppingCart) {
+        this.shoppingCart = shoppingCart;
+   }
+
+    public ArrayList<Gnome> getShoppingCart() {
+        return shoppingCart;
+    }
+     
+    
+    public void addCart(){
+       if(shoppingCart == null) shoppingCart = new ArrayList<Gnome>();
+       Gnome gnometemp;
+       gnometemp = new Gnome(gnometype,amount);
+      boolean trobat = false;
+      for(int i=0; i< shoppingCart.size(); i++){
+          if(shoppingCart.get(i).getName().equals(gnometype)) {
+              shoppingCart.get(i).setAmount(shoppingCart.get(i).getAmount() + amount);
+              trobat = true;
+          }
+      }
+      if(!trobat) shoppingCart.add(gnometemp);
+    }
+    
+    public void buy(){     
+        if(gwsFacade.buyGnome(gnometype, amount)) {
+            shoppingCart = new ArrayList<Gnome>();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("You successfully bought the gnomes!"));
+        }
+        else FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Some error occurred during the buying process"));
+    }
+    
+     public void setGnometype(String gnometype) {
+        this.gnometype = gnometype;
+    }
+
+    public String getGnometype() {
+        return gnometype;
+    }
+    
      /**
      * @return the username
      */
@@ -103,5 +159,14 @@ public class UserManager implements Serializable{
      */
     public void setUsername(String username) {
         this.username = username;
+    }
+
+       
+     public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getAmount() {
+        return amount;
     }
 }
