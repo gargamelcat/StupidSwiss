a9b658bd50d7855748d301d2fc12c621e94e99ff
heya87 <joel.rolli@gmail.com>
2014-01-06 11:35:42 +0100

    added sql statement to create gnomes

--- web/SQLStatements/CreateTable.sql
+++ web/SQLStatements/CreateTable.sql
@@ -1,18 +1,8 @@
-INSERT INTO ROOT.CONVERSIONRATE (CONVRATE, RESULTCURRENCY, ORIGINCURRENCY) 
-	VALUES  (0.72, 'EURO', 'DOLLAR'),
-	       	(0.88, 'CHF', 'DOLLAR'),
-		(6.57, 'SEK', 'DOLLAR'),
-	  	(1.37, 'DOLLAR', 'EURO'),
-	     	(1.12, 'DOLLAR', 'CHF'),
-	   	(0.15, 'DOLLAR', 'SEK'),
-	   	(0.81, 'EURO', 'CHF'),
-	  	(7.41, 'SEK', 'CHF'),
-	  	(1.22, 'CHF', 'EURO'),
-	 	(0.13, 'CHF', 'SEK'),
-	 	(6.57, 'SEK', 'EURO'),
-	 	(0.11, 'EURO', 'SEK'),
-	  	(1, 'CHF', 'CHF'),
-	 	(1, 'EURO', 'EURO'),
-	 	(1, 'DOLLAR', 'DOLLAR'),
-	 	(1, 'SEK', 'SEK')
-
+INSERT INTO ROOT.GNOME ("NAME", AMOUNT) 
+	VALUES ('bli', 10)
+INSERT INTO ROOT.GNOME ("NAME", AMOUNT) 
+	VALUES ('bla', 10)
+INSERT INTO ROOT.GNOME ("NAME", AMOUNT) 
+	VALUES ('blub', 10)
+INSERT INTO ROOT.GNOME ("NAME", AMOUNT) 
+	VALUES ('halo', 10)
